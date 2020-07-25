17e6011b6ad28c331d70e10a12778373a15893bf
Jeyakumaran Mayooresan <mayu3g@gmail.com>
2020-07-25 17:20:59 +0800

    initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import HomeScreen from './src/screens/HomeScreen';
+
+const App = (): any => {
+  return (
+      <HomeScreen/>
+  );
+};
+
+export default App;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Text, SafeAreaView} from 'react-native'
+
+const HomeScreen = (): any => {
+    return (
+        <SafeAreaView>
+            <Text>test </Text>   
+        </SafeAreaView>
+    )
+}
+
+export default HomeScreen
