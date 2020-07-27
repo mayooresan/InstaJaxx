3be3e216444d9f09cec2140406808fa956949ebd
Jeyakumaran Mayooresan <mayu3g@gmail.com>
2020-07-27 15:26:05 +0800

    styles moved to stylesheets

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,65 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { View, TouchableOpacity, Image, StyleSheet } from 'react-native'
 
 const Header = () => {
     return (
-        <View style={{ backgroundColor:'#FAFAFA'}}>
-        <View style={{height:32}}/>
-        <View style={{justifyContent:'center', alignItems:'center', flexDirection:'row', height:64}}>
-            <TouchableOpacity style={{width:'15%', justifyContent:'center', alignItems:'center'}}>
-                    <Image
-                        source={require('../../src/assets/images/camera.png')}
-                        style={{width:48, height:24, resizeMode: 'contain'}}
-                    />
-            </TouchableOpacity>
-            <View style={{width:'70%', justifyContent:'center', alignItems:'center'}}>
-                    <Image
-                        source={require('../../src/assets/images/instagram.png')}
-                        style={{width:200, height:32, resizeMode: 'contain'}}
-                    />
+        <View style={styles.container}>
+            <View style={styles.fillerView}/>
+            <View style={styles.innerView}>
+                <TouchableOpacity style={styles.imageButtonOne}>
+                        <Image
+                            source={require('../../src/assets/images/camera.png')}
+                            style={styles.actionButtonStyle}
+                        />
+                </TouchableOpacity>
+                <View style={styles.imageButtonTwo}>
+                        <Image
+                            source={require('../../src/assets/images/instagram.png')}
+                            style={styles.logoStyle}
+                        />
+                </View>
+                <TouchableOpacity style={styles.imageButtonThree}>
+                        <Image
+                            source={require('../../src/assets/images/messages.png')}
+                            style={styles.actionButtonStyle}
+                        />
+                </TouchableOpacity>
             </View>
-            <TouchableOpacity style={{width:'15%', justifyContent:'center', alignItems:'center'}}>
-                    <Image
-                        source={require('../../src/assets/images/messages.png')}
-                        style={{width:48, height:24, resizeMode: 'contain'}}
-                    />
-            </TouchableOpacity>
         </View>
-    </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor:'#FAFAFA'
+    }, fillerView: {
+        height:32
+    }, innerView: {
+        justifyContent:'center', 
+        alignItems:'center', 
+        flexDirection:'row', 
+        height:64
+    }, imageButtonOne: {
+        width:'15%', 
+        justifyContent:'center', 
+        alignItems:'center'
+    }, imageButtonTwo: {
+        width:'70%', 
+        justifyContent:'center', 
+        alignItems:'center'
+    }, imageButtonThree: {
+        width:'15%', 
+        justifyContent:'center', 
+        alignItems:'center'
+    }, actionButtonStyle: {
+        width:48, 
+        height:24, 
+        resizeMode: 'contain'
+    }, logoStyle: {
+        width:200, 
+        height:32, 
+        resizeMode: 'contain'
+    }
+});
 
 export default Header
--- src/components/ImageComponentFooter.tsx
+++ src/components/ImageComponentFooter.tsx
@@ -1,29 +1,29 @@
 import React from 'react'
-import { View, Text, Image, TouchableOpacity, SafeAreaView } from 'react-native'
+import { View, Text, Image, TouchableOpacity, SafeAreaView, StyleSheet } from 'react-native'
 
 const ImageComponentFooter = (props: {title: String, likes: Number, userName: String}) => {
     return (
-        <SafeAreaView style={{marginTop:16}}>
-             <View style={{flexDirection:'row'}}>
-                 <View style={{flexDirection:'row', width:'90%'}}>
+        <SafeAreaView style={styles.container}>
+             <View style={styles.primaryContainer}>
+                 <View style={styles.buttonGroupContainer}>
                         <TouchableOpacity>
                             <Image
                                 source={require('../../src/assets/images/heart.png')}
-                                style={{width:30, height:30, marginLeft:16, resizeMode: 'contain'}}
+                                style={styles.imageButtonStyle}
                             />
                         </TouchableOpacity>
                         
                         <TouchableOpacity>
                             <Image
                                 source={require('../../src/assets/images/comment.png')}
-                                style={{width:30, height:30, marginLeft:16, resizeMode: 'contain'}}
+                                style={styles.imageButtonStyle}
                             />
                         </TouchableOpacity>
                         
                         <TouchableOpacity>
                             <Image
                                 source={require('../../src/assets/images/messages.png')}
-                                style={{width:30, height:30, marginLeft:16, resizeMode: 'contain'}}
+                                style={styles.imageButtonStyle}
                             />
                         </TouchableOpacity>
                         
@@ -31,16 +31,42 @@ const ImageComponentFooter = (props: {title: String, likes: Number, userName: St
                  <TouchableOpacity>
                     <Image
                             source={require('../../src/assets/images/bookmark.png')}
-                            style={{width:30, height:30, resizeMode: 'contain'}}
+                            style={styles.imageButtonStyle}
                         />
                     </TouchableOpacity>
              </View>
-            <Text style={{margin:16, fontWeight: 'bold'}}>{props.likes} Likes</Text>
-            <View style={{flexDirection:'row', alignItems:'center', paddingLeft:16, paddingRight:16}}>
-                    <Text><Text style={{fontWeight: 'bold'}}>{props.userName}</Text> {props.title} </Text>
+            <Text style={styles.boldTextStyle}>{props.likes} Likes</Text>
+            <View style={styles.titleStyle}>
+                    <Text><Text style={styles.textStyle}>{props.userName}</Text> {props.title} </Text>
             </View>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop:16
+    }, primaryContainer: {
+        flexDirection:'row'
+    }, buttonGroupContainer: {
+        flexDirection:'row', 
+        width:'90%'
+    }, imageButtonStyle: {
+        width:30, 
+        height:30, 
+        marginLeft:16, 
+        resizeMode: 'contain'
+    }, boldTextStyle: {
+        margin:16, 
+        fontWeight: 'bold'
+    }, titleStyle: {
+        flexDirection:'row', 
+        alignItems:'center', 
+        paddingLeft:16, 
+        paddingRight:16
+    }, textStyle: {
+        fontWeight: 'bold'
+    }
+  });
 
 export default ImageComponentFooter
